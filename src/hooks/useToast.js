7e5aa3a1ad7d98d06f2290d71fdf0030d2f69c38
moneykrishnaeducation <moneykrishnaeducation@gmail.com>
2025-11-20 13:21:35 +0530

    toast notification added

---
+++ src/hooks/useToast.js
@@ -0,0 +1,26 @@
+import { useState, useCallback } from 'react';
+
+let toastId = 0;
+
+export const useToast = () => {
+  const [toasts, setToasts] = useState([]);
+
+  const addToast = useCallback((toast) => {
+    const id = ++toastId;
+    const newToast = { ...toast, id };
+    setToasts(prev => [...prev, newToast]);
+    return id;
+  }, []);
+
+  const removeToast = useCallback((id) => {
+    setToasts(prev => prev.filter(toast => toast.id !== id));
+  }, []);
+
+  return {
+    toasts,
+    addToast,
+    removeToast
+  };
+};
+
+export default useToast;
