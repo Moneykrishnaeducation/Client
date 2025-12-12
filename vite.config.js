dfcd69ac11dbaa61e8dddd629009fb62fdae509e
Twoface002 <tamizharasan344@gmail.com>
2025-12-12 13:52:00 +0530

    Compress

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import compression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    compression({ algorithm: 'gzip', ext: '.gz', threshold: 1024 }),
+    compression({ algorithm: 'brotliCompress', ext: '.br', threshold: 1024 })
+  ],
   base: "/static/clientPanel/",
   build: {
     outDir: '../static/clientPanel',
